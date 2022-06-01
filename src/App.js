abfc172d8ce9b3acf62fead7c562f615bbb0c647
nav87pow <76499348+nav87pow@users.noreply.github.com>
2022-06-01 14:26:36 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <main>
+      <h1>Hello CodeSandbox</h1>
+
+      <h2>Start editing to see some magic happen!</h2>
+
+      <section></section>
+      <div></div>
+      <table>
+        <th></th>
+        <th></th>
+        <th></th>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+
+      <aside></aside>
+
+      <footer></footer>
+    </main>
+  );
+};
+
+export default App;
